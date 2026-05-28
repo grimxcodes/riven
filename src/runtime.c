41475ca99550aca0a55f5f1ad6b758800df7a282
Ansh <grimxcodeskill@gmail.com>
2026-05-28 13:04:54 +0530

    Update runtime.c

--- src/runtime.c
+++ src/runtime.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "runtime.h"
+
+typedef struct {
+
+    char name[100];
+    char value[100];
+
+} Variable;
+
+Variable variables[100];
+
+int variable_count = 0;
 
 void runtime_stamp(char* text) {
 
     printf("%s\n", text);
+
+}
+
+void runtime_set_variable(char* name, char* value) {
+
+    strcpy(variables[variable_count].name, name);
+
+    strcpy(variables[variable_count].value, value);
+
+    variable_count++;
+
+}
+
+char* runtime_get_variable(char* name) {
+
+    for (int i = 0; i < variable_count; i++) {
+
+        if (strcmp(variables[i].name, name) == 0) {
+
+            return variables[i].value;
+
+        }
+    }
+
+    return "";
+
 }
