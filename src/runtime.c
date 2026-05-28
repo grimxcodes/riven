c327fd17b678b0fa0912c64c27be21813f3bf1ca
Ansh <grimxcodeskill@gmail.com>
2026-05-28 15:21:46 +0530

    Update runtime.c

--- src/runtime.c
+++ src/runtime.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "runtime.h"
 
@@ -11,31 +11,69 @@ typedef struct {
 
 } Variable;
 
-Variable variables[100];
+static Variable variables[1000];
 
-int variable_count = 0;
+static int variable_count = 0;
 
-void runtime_stamp(char* text) {
+void runtime_set_variable(
+    const char* name,
+    const char* value
+) {
 
-    printf("%s\n", text);
+    for (
+        int i = 0;
+        i < variable_count;
+        i++
+    ) {
 
-}
+        if (
+            strcmp(
+                variables[i].name,
+                name
+            ) == 0
+        ) {
 
-void runtime_set_variable(char* name, char* value) {
+            strcpy(
+                variables[i].value,
+                value
+            );
 
-    strcpy(variables[variable_count].name, name);
+            return;
 
-    strcpy(variables[variable_count].value, value);
+        }
+
+    }
+
+    strcpy(
+        variables[variable_count].name,
+        name
+    );
+
+    strcpy(
+        variables[variable_count].value,
+        value
+    );
 
     variable_count++;
 
 }
 
-char* runtime_get_variable(char* name) {
+char* runtime_get_variable(
+    const char* name
+) {
 
-    for (int i = 0; i < variable_count; i++) {
+    for (
+        int i = 0;
+        i < variable_count;
+        i++
+    ) {
 
-        if (strcmp(variables[i].name, name) == 0) {
+        if (
+            strcmp(
+                variables[i].name,
+                name
+            ) == 0
+        ) {
 
             return variables[i].value;
 
@@ -43,6 +81,14 @@ char* runtime_get_variable(char* name) {
 
     }
 
-    return "";
+    return "0";
+
+}
+
+void runtime_stamp(
+    const char* text
+) {
+
+    printf("%s\n", text);
 
 }
